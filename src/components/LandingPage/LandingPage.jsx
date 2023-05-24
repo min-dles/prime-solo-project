384ed318f80731a9ddcc38185bbe9c456c671636
mandi andrade <mandi.andrade20@gmail.com>
2023-05-24 16:29:16 -0500

    lunar phase packet installed and rendering to DOM

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
+// LUNAR PHASE: 
+import { Moon } from "lunarphase-js";
+
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome!');
   const history = useHistory();
+
+  const date = new Date();
+  const phase = Moon.lunarPhase(date);
 
   const onLogin = (event) => {
     history.push('/login');
@@ -16,6 +22,8 @@ function LandingPage() {
   return (
     <div className="container">
       <h2>{heading}</h2>
+      <h3>{phase}</h3>
+      
       <div className="grid">
         <div className="grid-col grid-col_8">
           <div class="first-quarter moon"></div>

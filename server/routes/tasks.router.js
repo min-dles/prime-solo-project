9dcb80a2174c63ffdab1525ace5033839fb6c754
mandi andrade <mandi.andrade20@gmail.com>
2023-05-29 18:31:46 -0500

    user auth needed for task GET

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 // const axios = require('axios');
-// const {
-//     rejectUnauthenticated,
-// } = require('../modules/authentication-middleware');
+const {
+    rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 // DB connection:
 const pool = require('../modules/pool');
 
 // GET route for user tasks: 
-router.get('/', (req, res) => {
-    console.log('GET /task-list');
-
+router.get('/', rejectUnauthenticated, (req, res) => {
     let sqlQuery = `
-        SELECT * FROM "user_todo";`;
+        SELECT * FROM "user_todo"
+        WHERE "user_id"=($1);`;
     
-    // let sqlValues = [req.user.id];
+    let sqlValues = [req.user.id];
 
-    pool.query(sqlQuery)
+    pool.query(sqlQuery, sqlValues)
     .then((results) => {
         console.log('this is results for tasks GET route:', results.rows);
         res.send(results.rows)

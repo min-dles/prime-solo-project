8bfe5144bb463139f3d456912ce96e29f157b559
mandi andrade <mandi.andrade20@gmail.com>
2023-05-29 22:35:29 -0500

    wrote and tested PUT route in postman

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -13,17 +13,17 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     let sqlQuery = `
         SELECT * FROM "user_todo"
         WHERE "user_id"=($1);`;
-    
+
     let sqlValues = [req.user.id];
 
     pool.query(sqlQuery, sqlValues)
-    .then((results) => {
-        console.log('this is results for tasks GET route:', results.rows);
-        res.send(results.rows)
-    }).catch((error) => {
-        console.log('error w task GET route:', error);
-        res.sendStatus(500);
-    })
+        .then((results) => {
+            console.log('this is results for tasks GET route:', results.rows);
+            res.send(results.rows)
+        }).catch((error) => {
+            console.log('error w task GET route:', error);
+            res.sendStatus(500);
+        })
 });
 
 // POST route for (auth'd/logged in) user's to add tasks to their DB list:
@@ -43,6 +43,27 @@ router.post('/', rejectUnauthenticated, (req, res) => {
             res.sendStatus(500);
         })
 });
+
+// PUT route to update tasks (to edit description, moon phase, and/or category)
+router.put('/:id', (req, res) => {
+    let taskUpdate = req.body.task;
+    let categoryUpdate = req.body.category;
+    let phaseUpdate = req.body.phase;
+    // req.params example: { id: '3' } with id referencing chore id (not user id)
+    let idToUpdate = req.params.id;
+    let sqlQuery = `
+        UPDATE "user_todo"
+            SET ("todo_description", "category_id", "moon_id")=($1,$2,$3)
+            WHERE "id"=$4;`;
+    let sqlValues = [taskUpdate, categoryUpdate, phaseUpdate, idToUpdate];
+    pool.query(sqlQuery, sqlValues)
+        .then((dbRes) => {
+            res.sendStatus(200);
+        }).catch((dbErr) => {
+            console.log('error with PUT task-list route:', dbErr);
+            res.sendStatus(500);
+        })
+})
 
 
 module.exports = router;

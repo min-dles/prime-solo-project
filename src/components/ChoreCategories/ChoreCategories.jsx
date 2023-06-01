ae059b1231672213036684d4d021ce36cc21de63
mandi andrade <mandi.andrade20@gmail.com>
2023-05-31 21:56:07 -0500

    refactored chore category btns for click handler

--- src/components/ChoreCategories/ChoreCategories.jsx
+++ src/components/ChoreCategories/ChoreCategories.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './ChoreCategories.css';
 
 function ChoreCategories() {
+
+    // make a variable with the chore categories to be easily called/mapped:
+    const categoriesArray = ['Household', 'Cleaning', 'Social', 'Documents', 'Health', 'Shopping'];
+
+    const dispatch = useDispatch();
+    // default state for the chore categories is empty string:
+    const [choreCategory, setChoreCategory] = useState('');
+
+    // listen for btn click & update state: 
+    const chooseCategory = (categoryName) => {
+        console.log('This category was clicked:', categoryName);
+    }
 
     return (
         <div className="container chore-categories">
 
             <h3>Chore Categories:</h3>
-
-            <button className="btn-styling">Household</button>
-            <button className="btn-styling">Cleaning</button>
-            <button className="btn-styling">Social</button>
-            <button className="btn-styling">Documents</button>
-            <button className="btn-styling">Health</button>
-            <button className="btn-styling">Shopping</button>
-            
+            {categoriesArray.map(category => {
+                return (
+                    <button
+                        key={category}
+                        onClick={() => chooseCategory(category)}
+                        className="btn-styling">
+                        {category}
+                    </button>
+                )
+            })}
         </div>
     )
 }
